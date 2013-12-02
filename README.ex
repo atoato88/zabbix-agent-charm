6beac7cc070de4fc500169804f0f0b1e22136679
myname <ubuntu@maas02.(none)>
2013-12-02 22:19:11 +0900

    use local zabbix-agent deb file. add dpkg --purge step in stop hook.

--- README.ex
+++ README.ex
@@ -1,14 +1,10 @@
-Describe the intended usage of this charm and anything unique about how
-this charm relates to others here. 
-
-This README will be displayed in the Charm Store, it should be either Markdown or RST. Ideal READMEs include instructions on how to use the charm, expected usage, and charm features that your audience might be interested in. For an example of a well written README check out Hadoop: http://jujucharms.com/charms/precise/hadoop
-
-Here's an example you might wish to template off of:
 
 Overview
 --------
 
-This charm provides (service) from (service homepage). Add a description here of what the service itself actually does. 
+this charm install zabbix-agent, but not create relationship with zabbix-server.
+for creating relationship with zabbix-server, you set zabbix-server-ip-address value in config.yaml file.
+so, this charm assume zabbix-server has created already.
 
 
 Usage
@@ -18,15 +14,11 @@ Step by step instructions on using the charm:
 
     juju deploy servicename
 
-and so on. If you're providing a web service or something that the end user needs to go to, tell them here, especially if you're deploying a service that might listen to a non-default port. 
-
-You can then browse to http://ip-address to configure the service. 
 
 Configuration
 -------------
 
-The configuration options will be listed on the charm store, however If you're making assumptions or opinionated decisions in the charm (like setting a default administrator password), you should detail that here so the user knows how to change it immediately, etc.
-
+See config.yaml file.
 
 Contact Information
 -------------------
